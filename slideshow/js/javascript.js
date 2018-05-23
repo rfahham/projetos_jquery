53cfec9cc140a9c69391e9daeabca1cc3230ccee
ricardo.fahham <ricardo.fahham@corp.globo.com>
2018-05-23 15:00:15 -0300

    Criando um slideshow

---
+++ slideshow/js/javascript.js
@@ -0,0 +1,28 @@
+$(function(){
+	$('#slide img:eq(0)').addClass('ativo').show();
+	var texto = $('.ativo').attr('alt');
+	$('#slide').prepend('<p>'+texto+'</p>');
+
+	// Temporizador
+	setInterval(slide,4000);
+
+
+	// Gerando o Slide
+	function slide (){
+
+		if($('.ativo').next().size()){
+
+			$('.ativo').fadeOut().removeClass('ativo').next().fadeIn().addClass('ativo');
+
+		}else{
+
+			$('.ativo').fadeOut().removeClass('ativo');
+			$('#slide img:eq(0)').fadeIn().addClass('ativo');
+		}
+
+		var texto = $('.ativo').attr('alt');
+		$('#slide p').hide().html(texto).delay(500).fadeIn();
+
+	}
+	
+});
